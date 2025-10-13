3afc611fcddb50771c08688cad1eaca975b02848
Mohamed Gamil <mohamedgamil0661672@gmail.com>
2025-10-13 21:22:39 +0300

    create all fetch methods

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,29 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl({required this.apiService});
+
   @override
-  Future<Either<Failure, List<BookModel>>> fecthBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fecthBestSellerBooks() async {
+    try {
+      final data = await apiService.get(
+        endPoint:
+            'volumes?q=flowers&orderBy=newest&key=AIzaSyCtN5ciqu4PhwN8RPu3lsjF2aQy3fRbusg',
+      );
+      List<BookModel> books = [];
+      for (var items in data['items']) {
+        books.add(BookModel.fromJson(items));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
