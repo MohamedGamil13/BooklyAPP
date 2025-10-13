8055b003e369bd6bf95c1be3be71c30196808290
Mohamed Gamil <mohamedgamil06616@gmail.com>
2025-10-13 12:12:44 +0300

    add app rating to book details

--- lib/Features/home/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
-        Icon(FontAwesomeIcons.solidStar, color: Color(0xffffdd4f)),
+        Icon(FontAwesomeIcons.solidStar, color: Color(0xffffdd4f), size: 14),
         const SizedBox(width: 6.3),
         Text(
           '4.8',
