70b4cab6ac3f4b4691ed67afe14630f485ea99ff
Mohamed Gamil <mohamedgamil06616@gmail.com>
2025-10-24 02:15:31 +0300

    Integrate RalevantBookCubit for fetching and displaying similar books in the UI

--- lib/Features/home/presentation/views/book_details_view.dart
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -1,14 +1,24 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/book_details_body.dart';
+import 'package:bookly_app/Features/home/presentation/views_model/ralevant_bookCuvit/ralevant_book_cubit_cubit.dart';
+import 'package:bookly_app/core/utils/sevices_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookDetailsView extends StatelessWidget {
   const BookDetailsView({super.key, required this.book});
   final BookModel book;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: BookDetailsBody(book: book)),
+    return BlocProvider(
+      create: (context) => RalevantBookCubitCubit(getIt.get<HomeRepoImpl>())
+        ..fetchRalevantBooks(
+          category: book.volumeInfo.categories?[0] ?? "Programming",
+        ),
+      child: Scaffold(
+        body: SafeArea(child: BookDetailsBody(book: book)),
+      ),
     );
   }
 }
