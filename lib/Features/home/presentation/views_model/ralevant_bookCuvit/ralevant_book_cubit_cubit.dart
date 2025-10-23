af8dddd21f65c6400fd108e0094d9792242a8d6c
Mohamed Gamil <mohamedgamil06616@gmail.com>
2025-10-24 01:34:20 +0300

    Add RalevantBookCubit and state management for fetching relevant books

---
+++ lib/Features/home/presentation/views_model/ralevant_bookCuvit/ralevant_book_cubit_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'ralevant_book_cubit_state.dart';
+
+class RalevantBookCubitCubit extends Cubit<RalevantBookCubitState> {
+  RalevantBookCubitCubit(this.repo) : super(RalevantBookCubitInitial());
+  final HomeRepo repo;
+  Future<void> fetchRalevantBooks({required String category}) async {
+    emit(RalevantBookCubitLoading());
+    var res = await repo.fecthNewstBooks();
+    res.fold(
+      (failure) {
+        emit(RalevantBookCubitFailure(failure.errorMassege));
+      },
+      (books) {
+        emit(RalevantBookCubitSuccess(books));
+      },
+    );
+  }
+}
