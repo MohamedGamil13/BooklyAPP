1d920fcdad167a2b10bed4801889de96fd8f6048
Mohamed Gamil <mohamedgamil06616@gmail.com>
2025-10-23 20:42:07 +0300

    display data and create singlton class

--- lib/Features/home/presentation/views/widgets/featured_films_listview.dart
+++ lib/Features/home/presentation/views/widgets/featured_films_listview.dart
@@ -1,24 +1,36 @@
-import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_films_item.dart';
+import 'package:bookly_app/Features/home/presentation/views_model/featured_bookCubit/featured_books_cubit.dart';
+import 'package:bookly_app/core/widgets/custom_progress_indector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedFilmsListview extends StatelessWidget {
   const FeaturedFilmsListview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print('==================================${books}');
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.28,
-
-      child: ListView.builder(
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: FeaturedFilmsItem(),
-        ),
-        itemCount: books.length,
-        scrollDirection: Axis.horizontal,
-      ),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSucess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.28,
+            child: ListView.builder(
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: FeaturedFilmsItem(
+                  imageUrl: state.books[index].volumeInfo.imageLinks.thumbnail,
+                ),
+              ),
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+            ),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return Text('error');
+        } else {
+          return CustomProgressIndector();
+        }
+      },
     );
   }
 }
--- lib/Features/home/presentation/views/widgets/simliar_books_section.dart
+++ lib/Features/home/presentation/views/widgets/simliar_books_section.dart
@@ -24,7 +24,10 @@ class SimliarBooksSection extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemBuilder: (context, index) => Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 3),
-                child: const FeaturedFilmsItem(),
+                child: const FeaturedFilmsItem(
+                  imageUrl:
+                      'https://tse1.mm.bing.net/th/id/OIP.oqNwChrb_h42KYxCQ_TcDAHaLG?w=683&h=1024&rs=1&pid=ImgDetMain&o=7&rm=3',
+                ),
               ),
             ),
           ),
