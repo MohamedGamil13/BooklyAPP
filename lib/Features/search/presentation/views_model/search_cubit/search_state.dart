859c239cc632ef7cef97ab87fe09b83477d521d0
Mohamed Gamil <mohamedgamil0661672@gmail.com>
2025-11-08 18:08:43 +0200

    Refactor SearchCubit and SearchState to implement book search functionality with proper state management

--- lib/Features/search/presentation/views_model/search_cubit/search_state.dart
+++ lib/Features/search/presentation/views_model/search_cubit/search_state.dart
@@ -11,7 +11,14 @@ final class SearchInitial extends SearchState {}
 
 final class Searchloading extends SearchState {}
 
-final class SearchSuccess extends SearchState {}
+final class SearchSuccess extends SearchState {
+  final List<BookModel> books;
+
+  const SearchSuccess({required this.books});
+
+  @override
+  List<Object> get props => [books];
+}
 
 final class SearchFailure extends SearchState {
   final String errMessage;
