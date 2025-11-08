fed7c7b9ac431d05988942fc496ea21ae9a85853
Mohamed Gamil <mohamedgamil06616@gmail.com>
2025-11-08 18:08:43 +0200

    Refactor SearchCubit and SearchState to implement book search functionality with proper state management

--- lib/Features/search/presentation/views_model/search_cubit/search_cubit.dart
+++ lib/Features/search/presentation/views_model/search_cubit/search_cubit.dart
@@ -1,8 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/search/data/repos/srearch_repo_impl.dart';
 import 'package:equatable/equatable.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  SearchCubit(this.repo) : super(SearchInitial());
+  final SrearchRepoImpl repo;
+  Future<void> searchBooks(String category) async {
+    emit(Searchloading());
+    try {
+      final res = await repo.getBookByCategory(category);
+      res.fold(
+        (failure) => emit(SearchFailure(errMessage: failure.errorMassege)),
+
+        (books) => emit(SearchSuccess(books: books)),
+      );
+    } catch (e) {
+      emit(SearchFailure(errMessage: e.toString()));
+    }
+  }
 }
