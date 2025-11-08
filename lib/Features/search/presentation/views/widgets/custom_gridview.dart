1b23109b98214a76c7482753cf60cb3b8a13e5d1
Mohamed Gamil <mohamedgamil06616@gmail.com>
2025-11-08 20:01:21 +0200

    Refactor CustomGridview to display search results with dynamic book data; enhance UI layout and navigation

--- lib/Features/search/presentation/views/widgets/custom_gridview.dart
+++ lib/Features/search/presentation/views/widgets/custom_gridview.dart
@@ -1,9 +1,12 @@
+import 'package:bookly_app/Features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_films_item.dart';
 import 'package:bookly_app/Features/search/presentation/views_model/search_cubit/search_cubit.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/core/widgets/custom_progress_indector.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class CustomGridview extends StatelessWidget {
   const CustomGridview({super.key});
@@ -13,18 +16,27 @@ class CustomGridview extends StatelessWidget {
     return BlocBuilder<SearchCubit, SearchState>(
       builder: (context, state) {
         if (state is SearchSuccess) {
-          return GridView.builder(
-            padding: EdgeInsets.zero,
-            itemCount: 10,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 12,
-              childAspectRatio: 0.6,
-            ),
-            itemBuilder: (context, index) => FeaturedFilmsItem(
-              imageUrl:
-                  'https://tse4.mm.bing.net/th/id/OIP.rb6aTeGCgJp_H4hAi-DMCwHaL0?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3',
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: GridView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: state.books.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 12,
+                childAspectRatio: 0.7,
+              ),
+              itemBuilder: (context, index) => GestureDetector(
+                onTap: () {
+                  Get.to(() => BookDetailsView(book: state.books[index]));
+                },
+                child: FeaturedFilmsItem(
+                  imageUrl:
+                      state.books[index].volumeInfo.imageLinks?.thumbnail ??
+                      'https://tse1.mm.bing.net/th/id/OIP.9AxhSpKqIYF7imxoBkOOFgHaL2?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3',
+                ),
+              ),
             ),
           );
         } else if (state is SearchFailure) {
