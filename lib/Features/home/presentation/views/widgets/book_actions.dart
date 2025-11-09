be47d25183b17b74697508fe4f861ff4968ea987
Mohamed Gamil <mohamedgamil0661672@gmail.com>
2025-11-09 17:16:10 +0200

    Add URL launcher functionality to BookActions; update CustomTextButton to accept onPressed callback

--- lib/Features/home/presentation/views/widgets/book_actions.dart
+++ lib/Features/home/presentation/views/widgets/book_actions.dart
@@ -1,28 +1,32 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BookActions extends StatelessWidget {
-  const BookActions({super.key});
-
+  const BookActions({super.key, required this.book});
+  final BookModel book;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Expanded(
-          child: const CustomTextButton(
+          child: CustomTextButton(
             fontWeight: FontWeight.bold,
             text: 'Free',
             backGroundColor: Colors.white,
             textColor: Colors.black,
+
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(12),
               bottomLeft: Radius.circular(12),
             ),
+            onPressed: () {},
           ),
         ),
         Expanded(
-          child: const CustomTextButton(
+          child: CustomTextButton(
             fontWeight: FontWeight.w500,
             text: 'Free Preview',
             backGroundColor: Color(0xffef8262),
@@ -31,9 +35,19 @@ class BookActions extends StatelessWidget {
               topRight: Radius.circular(12),
               bottomRight: Radius.circular(12),
             ),
+            onPressed: () {
+              openExternalUrl(book.volumeInfo.previewLink!);
+            },
           ),
         ),
       ],
     );
+  }
+}
+
+Future<void> openExternalUrl(String link) async {
+  final Uri url = Uri.parse(link);
+  if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+    throw 'Could not launch $url';
   }
 }
